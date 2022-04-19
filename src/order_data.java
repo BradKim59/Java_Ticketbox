a225f57f871a8a94429c9c225a324393a6e777ad
jihyeon nam <36907154+JihyeonNam-0327@users.noreply.github.com>
2022-04-19 16:04:57 +0900

    구조 일부 변경 - Main 처리 삭제

--- src/order_data.java
+++ src/order_data.java
@@ -1,20 +1,60 @@
-package Ticket;
-
 public class order_data {
-	int ticket;
-	int dayTicket;
-	int qty = 1;
-	int advantage;
-	int numDaDoong;
+	private int ticket;
+	private int dayTicket;
+	public int qty = 1;
+	public int advantage;
+	public int numDaDoong;
 	//계산 시 변수 선언 
-	int disAdvantage;
-	int generation;
-	int discountType;
-	int price;
-	int totalPrice;
-	int totalDiscountPrice;
-	int grandPrice;
-	int grandSum;
-	int num;
-	int disNum;
+	public int disAdvantage;
+	public int generation;
+	public int discountType;
+	public int price;
+	public int totalPrice;
+	public int totalDiscountPrice;
+	public int grandPrice;
+	public int grandSum;
+	public int num;
+	public int disNum;
+	private int gender;
+	public int year;
+	public int month;
+	public int date;
+	public int four_digit_year;
+	
+	
+	
+	public int getTicket() {
+		return ticket;
+	}
+	public void setTicket(int ticket) {
+		this.ticket = ticket;
+	}
+	public int getDayTicket() {
+		return dayTicket;
+	}
+	public void setDayTicket(int dayTicket) {
+		this.dayTicket = dayTicket;
+	}
+	public int getGender() {
+		return gender;
+	}
+	public void setGender(int gender) {
+		if(gender < 1 || gender > 2) {
+			return;
+		}
+		this.gender = gender;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
