018375651a428482f26da50fd8f62933ef7f57c2
BradKim59 <httpksynet@naver.com>
2022-04-19 13:37:32 +0900

    Make Classes for variable

---
+++ src/order_data.java
@@ -0,0 +1,20 @@
+package Ticket;
+
+public class order_data {
+	int ticket;
+	int dayTicket;
+	int qty = 1;
+	int advantage;
+	int numDaDoong;
+	//계산 시 변수 선언 
+	int disAdvantage;
+	int generation;
+	int discountType;
+	int price;
+	int totalPrice;
+	int totalDiscountPrice;
+	int grandPrice;
+	int grandSum;
+	int num;
+	int disNum;
+}
